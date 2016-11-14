5b651970199be81d7912935fe6421c7453e7786f
maximilian.koeller <ralfrabbit@gmail.com>
2016-11-14 11:47:25 +0100

    added masterData router

---
+++ ksn_server/src/router/masterData.ts
@@ -0,0 +1,84 @@
+/**
+ * Created by maximilian.koeller on 14.11.2016.
+ */
+import * as mysql from "mysql";
+import * as wrapper from "node-mysql-wrapper";
+import * as express from "express";
+
+
+class MasterData {
+
+    public ksnDB = wrapper.wrap(mysql.createConnection({
+        host: "intranet",
+        user: 'FS141_maxi_koel',
+        password: 'FS141_maxi_koel',
+        database: 'fs141_maximilian_koeller',
+        insecureAuth: true
+    }));
+
+    public router: express.Router;
+
+    constructor() {
+        this.router = express.Router();
+        this.router.get("/listKlassen", listKlassen);
+        this.router.get("/listFaecher", listFaecher);
+        this.router.get("/listLehrer", listLehrer);
+        this.router.get("/listSchueler", listSchueler);
+    }
+}
+
+let masterData = new MasterData();
+
+function listKlassen(req: express.Request, res: express.Response): void {
+    let db = masterData.ksnDB;
+    db.ready(()=> {
+        db.query("select * from klasse", (err, response) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(response);
+            }
+        });
+    });
+}
+
+function listFaecher(req: express.Request, res: express.Response): void {
+    let db = masterData.ksnDB;
+    db.ready(()=> {
+        db.query("select * from fachnotenliste where klasse = ?", (err, response) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(response);
+            }
+        }, [req.query.klasse]);
+    });
+}
+
+function listLehrer(req: express.Request, res: express.Response): void {
+    let db = masterData.ksnDB;
+    db.ready(() => {
+        db.query("select * from lehrer", (err, response) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(response);
+            }
+        });
+    });
+}
+
+function listSchueler(req: express.Request, res: express.Response): void {
+    let db = masterData.ksnDB;
+    db.ready(() => {
+        db.query("select * from schueler where klasse = ?", (err, response) => {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(response);
+            }
+        },[req.query.klasse]);
+    });
+}
+
+export = masterData.router;
