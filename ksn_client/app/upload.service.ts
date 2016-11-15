568dab7607cb9d5d2f9da8d599583c05b88397ac
maximilian.koeller <ralfrabbit@gmail.com>
2016-11-15 14:37:19 +0100

    gradeUpload angulared

--- ksn_client/app/upload.service.ts
+++ ksn_client/app/upload.service.ts
@@ -24,7 +24,7 @@ export class UploadService {
     }
 
     private handleError(error: Response | any) {
-        // In a real world app, we might use a remote logging infrastructure
+        //In a real world app, we might use a remote logging infrastructure
         let errMsg: string;
         if (error instanceof Response) {
             const body = error.json() || '';
@@ -33,7 +33,7 @@ export class UploadService {
         } else {
             errMsg = error.message ? error.message : error.toString();
         }
-        console.error(errMsg);
+        console.error(error);
         return Observable.throw(errMsg);
     }
 }
