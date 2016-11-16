21582a64842d09872d8181a7940b828c1981718e
maximilian.koeller <ralfrabbit@gmail.com>
2016-11-16 07:30:27 +0100

    move commit

--- ksn_client/app/app.component.ts
+++ ksn_client/app/app.component.ts
@@ -1,11 +1,21 @@
 /**
  * Created by maximilian.koeller on 11.11.2016.
  */
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MasterLoaderService} from "./master-loader.service";
 @Component({
     selector: 'ksn-app',
-    templateUrl: '../menu.html'
+    templateUrl: '../menu.html',
+    providers: [MasterLoaderService]
 })
-export class AppComponent {
-    klasse : string = "FS141";
+export class AppComponent implements OnInit{
+    klasse : string;
+    klassen : string [];
+    showUpload : boolean = false;
+
+    constructor(private masterLoader : MasterLoaderService){}
+
+    ngOnInit() {
+        this.masterLoader.loadClasses().subscribe(res => this.klassen = res);
+    }
 }
