9a0e9a4b2808b796020ddbbabd5f8bae703144f2
spike-rabbit <ralfrabbit@gmail.com>
2016-11-18 15:09:14 +0100

    added db config

---
+++ ksn_server/src/database-configuration.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Maxi- PC on 18.11.2016.
+ */
+export class DatabaseConfiguration {
+    host: "intranet";
+    user: 'FS141_maxi_koel';
+    password: 'FS141_maxi_koel';
+    database: 'fs141_maximilian_koeller';
+    insecureAuth: true;
+}
